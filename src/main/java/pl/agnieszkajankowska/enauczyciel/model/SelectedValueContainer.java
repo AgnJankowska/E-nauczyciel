5ea70e051a0359497060d0ab59be06072dd62126
Agnieszka Jankowska <agn_jankowska@wp.pl>
2022-05-13 18:26:26 +0200

    adding model file - SelectedValueContener to transfer Section and Subject object between templates

---
+++ src/main/java/pl/agnieszkajankowska/enauczyciel/model/SelectedValueContainer.java
@@ -0,0 +1,14 @@
+package pl.agnieszkajankowska.enauczyciel.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class SelectedValueContainer {
+
+    private Subject subject;
+    private Section section;
+}
