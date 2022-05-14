4bf699a6a068a1cb7458b4704b889c3658078bbb
Agnieszka Jankowska <agn_jankowska@wp.pl>
2022-05-14 17:43:24 +0200

    creating SubjectViewController with get mapping

---
+++ src/main/java/pl/agnieszkajankowska/enauczyciel/controller/SubjectViewController.java
@@ -0,0 +1,48 @@
+package pl.agnieszkajankowska.enauczyciel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.agnieszkajankowska.enauczyciel.model.SelectedValueContainer;
+import pl.agnieszkajankowska.enauczyciel.model.Subject;
+import pl.agnieszkajankowska.enauczyciel.service.SubjectService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequestMapping("teacher/subjectsView")
+@Controller
+public class SubjectViewController {
+
+    @Autowired
+    private SubjectService service;
+
+    @GetMapping
+    String readSubject(Model model) {
+        addModelAttribute(model);
+        return "subjectsView";
+    }
+
+    @PostMapping(params="action=edit")
+    String editSubject(
+            @ModelAttribute("subject") @Valid Subject subject,
+            BindingResult bindingResult,
+            Model model
+    ) {
+///
+        return "subjectsView";
+    }
+
+    @ModelAttribute("subjectsList")
+    List<Subject> getSubject() {
+        return service.readAll();
+    }
+
+    private void addModelAttribute(Model model) {
+        model.addAttribute("subject", new Subject());
+        model.addAttribute("container", new SelectedValueContainer());
+        model.addAttribute("subjectsList", getSubject());
+    }
+}
