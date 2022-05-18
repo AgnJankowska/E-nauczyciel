0ef7c2c60f551a95bc2a8da4054a7316d9a6710d
Agnieszka Jankowska <agn_jankowska@wp.pl>
2022-05-18 13:00:40 +0200

    creating AssignmentLearningController with get mapping

---
+++ src/main/java/pl/agnieszkajankowska/enauczyciel/controller/AssignmentLearningController.java
@@ -0,0 +1,39 @@
+package pl.agnieszkajankowska.enauczyciel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.agnieszkajankowska.enauczyciel.model.Assignment;
+import pl.agnieszkajankowska.enauczyciel.model.Section;
+import pl.agnieszkajankowska.enauczyciel.model.SelectedValueContainer;
+import pl.agnieszkajankowska.enauczyciel.model.Subject;
+import pl.agnieszkajankowska.enauczyciel.service.AssignmentService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("student/learn/subjects")
+public class AssignmentLearningController {
+
+    @Autowired
+    private AssignmentService service;
+
+    @GetMapping("/sections/assignments")
+    public String readFirstAssignmentBySection(
+            @ModelAttribute("container") SelectedValueContainer container,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        model.addAttribute("assignment", service.getFirstAssignmentOrNullBySection(container.getSection()));
+        addModelAttribute(model, container.getSection());
+        return "assignmentsLearning";
+    }
+
+    private void addModelAttribute(Model model, Section section) {
+        model.addAttribute("section", section);
+        model.addAttribute("subject", section.getSubject());
+        model.addAttribute("assignmentsList", getAssignmentBySection(section));
+    }
+}
