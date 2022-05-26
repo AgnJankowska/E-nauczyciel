5b170fa41ef3674506b757861095fa0ede52fe9d
Agnieszka Jankowska <agn_jankowska@wp.pl>
2022-05-26 11:42:19 +0200

    creating UserDetailsServiceImpl and WebSecurityConfig

---
+++ src/main/java/pl/agnieszkajankowska/enauczyciel/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package pl.agnieszkajankowska.enauczyciel.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.agnieszkajankowska.enauczyciel.service.UserService;
+
+import java.util.Optional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        service.initializeUsersTableWithBasicRecord();
+
+        Optional<User> user = service.findByLogin(username);
+        MyUserDetails(user.get());
+    }
+}
